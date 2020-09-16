b809f222ffea0e3bf11f515890d4dbdac6ffd3c3
Fabiana Silva <fsffyks1978@gmail.com>
2020-09-16 20:54:29 -0300

    Initial commit

---
+++ app/src/main/java/com/example/tarea5/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.tarea5;
+package com.fabiana.Tarea 5;
+
+import android.content.Context;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("com.fabiana.Tarea 5", appContext.getPackageName());
+    }
+}}
+        package coma.fabiana.Tarea 5;
+
+        import android.content.Context;
+
+        import androidx.test.platform.app.InstrumentationRegistry;
+        import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+        import org.junit.Test;
+        import org.junit.runner.RunWith;
+
+        import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+        package co.nandocl.petagram;
+
+        import android.content.Context;
+
+        import androidx.test.platform.app.InstrumentationRegistry;
+        import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+        import org.junit.Test;
+        import org.junit.runner.RunWith;
+
+        import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("co.nandocl.petagram", appContext.getPackageName());
+    }
+}
+© 2020 GitHub, Inc.
+
+
